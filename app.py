b06e169df5e5787328819e2a8b2398d2acd8125f
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-10-13 19:13:51 -0500

    Git Flask Setup:                                               Initial main page configuration, many changes to make sharing for sycronicity with the teams parralel developments

--- app.py
+++ app.py
@@ -2,3 +2,32 @@
 Main Location to develop code
 
 """
+
+from flask import Flask, render_template, request  # NOT the same as requests 
+from apis import car_recall_api, photo_api, video_api, shops_api
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('car_input.html')
+
+
+#create the function to respond to requests from the HTML form 
+@app.route('/get_car')  #must match the call url exactly
+def get_car_recall():
+
+    # Convert into car model
+    year = request.args.get('year')
+    make = request.args.get('make')
+    model = request.args.get('model')
+    car = {'year': year, 'make': make, 'model': model }
+
+    car_recalls = 
+
+    return render_template('github.html', user_data=user_data)
+
+    
+
+if __name__ == '__main__':
+    app.run()
