a1c9aa0fb899f8301d528a7862ef64bebe9585df
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-10-22 07:32:35 -0500

    Bookmark PeeWee:  creating the initial database and setting the return to a big giant dictionary to be reconstructed for bookmarked.html

--- app.py
+++ app.py
@@ -40,6 +40,15 @@ def book_marked_page():
     return render_template('book_marked.html')
 
 
+@app.route('/save_top_recall', methods=['POST'])
+def save_bookmark():
+    save_request_data = request.form.to_dict()
+    print(save_request_data)
+    pretend_database.add_new_bookmark(save_request_data)
+    all_bookmarks = pretend_database.get_all_bookmarks()
+    return render_template('bookmarks.html', bookmarks=all_bookmarks)
+
+
 
 if __name__ == '__main__':
     app.run()
--- database/recall.py
+++ database/recall.py
@@ -1,22 +1,62 @@
 from peewee import *
 
-db = SqliteDatabase('car_recall_bookmarks.sqlite')
+db = SqliteDatabase('top_recall_bookmarks.sqlite')
 
 class Car_Recall(Model):
-    year = IntegerField()
+    # save car
+    year =CharField()
     make = CharField()
     model = CharField()
-
+# save photo 
     photo_title = CharField()
     photo_link = CharField()
     photo_error = CharField()
-
-    
-
-
+#  Save Recall
+    recall_date = CharField()
+    recall_component = CharField()
+    recall_summary = CharField()
+    recall_error = CharField()
+# <!-- Save the video -->
+    video_title = CharField()
+    video_embed = CharField()
+    video_error = CharField()
+# <!-- Save the shop -->
+    shop_url = CharField()
+    shop_name = CharField()
+    shop_rating = CharField()
+    shop_address = CharField()
+    shop_city = CharField()
+    shop_state = CharField() 
+    shop_error = CharField()
 
     class Meta:
         database = db
 
-    def get_recall(self):
-        return f'{self.id}, {self.name}, {self.color}, {self.age}'
+    def get_recalls():
+        bookmarks = Car_Recall.select()
+        for bookmark in bookmarks:
+            return {
+                # return car
+                {'year': bookmark['year']},
+                {},
+                {},
+                # return photo
+                {},
+                {},
+                {},
+                # return recall
+                {},
+                {},
+                {},
+                # return video
+                {},
+                {},
+                {},
+                # return shop
+                {},
+                {},
+                {},
+            }
+
+db.connect()
+db.create_tables([Car_Recall])
