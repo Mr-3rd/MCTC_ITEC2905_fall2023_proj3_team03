681096e68cc6baa8d344a48964f373f4b020e4c7
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-10-25 14:41:40 -0500

    begin video unit testing

--- Tests/test_video_api.py
+++ Tests/test_video_api.py
@@ -1,32 +1,33 @@
-"""
+'''
+Test recall videos
 
-"""
-from datetime import datetime
-import video_api
+'''
+import unittest
+from unittest import TestCase
 
-car = {'year': '2012', 'make': 'Fiat', 'model': '500'}
+import sys
+sys.path.append('..')
+from apis import video_api
 
-recall_data = {'count': 4, 'recalls': [{'ReportReceivedDate': '10/11/2011', 'Component': 'Service Brakes, Hydraulic:Fluid', 'Summary': 'Chrysler is recalling certain model year 2012 dodge journey and fiat 500 vehicles manufactured from october 24, 2011, through october 26, 2011.  some vehicles were assembled with contaminated brake fluid and may experience a degradation of the sealing components within the brake system.'},
- {'ReportReceivedDate': '03/07/2019', 'Component': 'Power Train:Manual Transmission:Floor Shift Assembly', 'Summary': 'Dlt, llc. is recalling certain cravenspeed fiat short shifter shafts sold for use in 2011-2019 fiat 500 vehicles.  the shifter shaft may have been improperly manufactured with a through hole for the set screw rather than a blind hole, which may lead to the breakage of the shifter shaft.'},
- {'ReportReceivedDate': '16/05/2016', 'Component': 'Power Train:Manual Transmission', 'Summary': 'Chrysler (fca us llc) is recalling certain model year 2012-2016 fiat 500 vehicles manufactured june 21, 2010, through january 12, 2016, equipped with a manual transmission.  the clutch diaphragm spring may fracture and fail, causing an inability to switch gears.'},
- {'ReportReceivedDate': '15/11/2019', 'Component': 'Power Train:Automatic Transmission:Lever And Linkage:Floor Shift', 'Summary': 'Chrysler (fca us llc) is recalling certain 2012-2013 fiat 500 vehicles equipped with 6-speed automatic transmissions.  the shifter cable bushing may fail allowing the cable to detach from the transmission.'}] }
 
-def display_recalls(info):
-    # print(info['count'])
-    # recalls = info['recalls']
+class GetCarRecallTest(TestCase):
 
-    # print(type(recalls))
+    def test_valid_12_Fiat_500(self): 
+        expected = (None, [{'title': 'Easy Transmission Shift Cable Bushing Replacement: HOW TO ESCAPE', 'embed': 'https://www.youtube.com/embed/G4V32X0XSxU'}, 
+                           {'title': 'Fiat 500 manual transmission clutch lockout switch replacement, repair starting issues', 
+                            'embed': 'https://www.youtube.com/embed/a7RfhDbWYyI'},
+                              {'title': 'Fiat 500 owners complain about dangerous clutch problems',
+                             'embed': 'https://www.youtube.com/embed/S_AXrs179Lc'},
+                               {'title': 'SYMPTOMS OF A BAD BRAKE MASTER CYLINDER', 'embed': 'https://www.youtube.com/embed/8eL1FZf5Oes'}])
+        
+        self.assertEqual(expected, video_api.get_car_videos('2012', 'fiat', '500'))
 
-    
+    def test_empty_input(self):
+        expected = ('Car recall videos not found', None)
+        self.assertEqual(expected, video_api.get_car_videos('', '', ''))
 
-    for recall in info:
-        print(recall)
 
-recall_data['recalls'].sort(key = lambda x: datetime.strptime(x['ReportReceivedDate'], '%d/%m/%Y'), reverse=True)
 
-print(recall_data['recalls'])
-info = video_api.get_car_videos(car, recall_data)
 
-print(info)
-
-# display_recalls(info)
+if __name__ == '__main__':
+    unittest.main()
